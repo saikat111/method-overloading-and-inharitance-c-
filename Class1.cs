4ef8962b461b5038a37d95bbd585ae8b323692ae
al rakib sorder <46457169+saikat111@users.noreply.github.com>
2019-12-22 00:56:11 +0600

    Add files via upload

---
+++ Class1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Overload
+{
+    class Class1
+    {
+        public void P()
+        {
+            Console.WriteLine("1st");
+        }
+    }
+    class Class2:Class1
+    {
+        public void Pp()
+        {
+            Console.WriteLine("2st");
+        }
+    }
+    class Class3:Class2
+    {
+        public void Ppp()
+        {
+            Console.WriteLine("3st");
+        }
+    }
+    class Demo
+    {
+        public static void main(String[] args)
+        {
+            Class3 c3 = new Class3();
+            c3.Ppp();
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Overload
+{
+    class A
+    {
+        public void p(int a)
+        {
+            Console.WriteLine("1st method");
+        }
+        public void p(int a,int b)
+        {
+            Console.WriteLine("2st method");
+        }
+        public void p(int a,int b,int c)
+        {
+            Console.WriteLine("3rd method");
+        }
+
+
+
+    }
+
+
+    class Mainn
+    {
+        static void Main(string[] args)
+        {
+            A a = new A();
+            a.p(2);
+            a.p(2,5);
+            a.p(4,8,7);
+        }
+    }
+}
